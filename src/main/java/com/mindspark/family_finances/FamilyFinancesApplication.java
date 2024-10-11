abf9fc8a0446d6c190f44fec7e152d580ac6acd6
Illia Korniichyk <korniykhik3@gmail.com>
2024-10-11 13:05:14 +0300

    init commit

---
+++ src/main/java/com/mindspark/family_finances/FamilyFinancesApplication.java
@@ -0,0 +1,13 @@
+package com.mindspark.family_finances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyFinancesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mindspark/family_finances/FamilyFinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindspark.family_finances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyFinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
